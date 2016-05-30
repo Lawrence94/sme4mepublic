fa0bb344dbf6c1ce346ebb580710b0d75bafb9c2
Lawrence Agbani <L.agbani@yahoo.com>
2016-05-30 16:05:55 +0100

    Fixed payment page

--- application/modules/Main/views/login/signup.php
+++ application/modules/Main/views/login/signup.php
@@ -94,6 +94,19 @@
 <script>
   SiteBackground.loadFromParam(null)
 </script>
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78546622-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <div class="sign-up"> <span>Already have an account?</span> <a class="button translucent" href="<?= empty($url)? site_url('login') : site_url('login/'.$url); ?>">Return to Login</a> </div>
 <div class="overlay"></div>
 </body>
